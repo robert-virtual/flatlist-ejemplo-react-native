40a1db4e918e1ed470bbea071f24e4973db5c9c5
robert-virtual <robertocastill63@gmail.com>
2022-04-08 16:42:07 -0600

    done

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import { useState, useEffect } from "react";
+import axios from "axios";
+axios.defaults.baseURL = "http://192.168.0.19:3030/api";
 
 export default function App() {
+  // cada q hay un cambio en la UI
+  useEffect(() => {
+    // solo al inicio
+    obtenerProductos();
+  }, []);
+  const [productos, setproductos] = useState([]);
+  async function obtenerProductos() {
+    const { data } = await axios.get("/productos");
+    console.log(data);
+    setproductos(data);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        data={productos}
+        renderItem={({ item }) => (
+          <View style={{ padding: 15 }}>
+            <Text>{item.nombre}</Text>
+            <Text>Lps.{item.precio}</Text>
+          </View>
+        )}
+        keyExtractor={(item, i) => i.toString()}
+      />
     </View>
   );
 }
@@ -13,8 +36,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
